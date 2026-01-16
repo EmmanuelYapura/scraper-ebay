a6e409d92b3a3be60371d062273335f4edd09124
Emmanuel Yapura <eyapura96@gmail.com>
2026-01-16 19:43:11 -0300

    convierte script asincrono

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from playwright.sync_api import sync_playwright
+import asyncio
 import json
+from playwright.async_api import async_playwright
 
 def crear_json(datos, producto):
     with open(f"{producto}.json", "w", encoding="utf-8") as file :
         json.dump(datos, file, indent=3, ensure_ascii=False)
 
-def extraer_info(productos, cant):
+async def extraer_info(productos, cant):
     data = []    
     for i in range(cant):
         product = productos.nth(i)
 
         title_locator = product.locator(".s-card__title span.primary").filter(visible=True).first
-        if title_locator.count() == 0:
+        if await title_locator.count() == 0:
             continue
 
-        title = title_locator.text_content()
+        title = await title_locator.text_content()
 
         price_locator = product.locator(".s-card__price")
         price = None 
 
-        if price_locator.count() > 0:
-            price_text = price_locator.first.text_content()
+        if await price_locator.count() > 0:
+            price_text = await price_locator.first.text_content()
             if price_text:
                 price = price_text
 
@@ -35,38 +36,41 @@ def extraer_info(productos, cant):
 
     return data
 
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.ebay.com/")
-
-    buscador = page.get_by_placeholder("Buscar artículos")
-    buscador.fill("ps4 2tb")
-    buscador.press("Enter")
-
-    page.wait_for_selector(".srp-results", state="visible")
-    
-    productos = page.locator(".srp-results > li")
-    cant_productos = productos.count()
-
-    paginacion = page.locator("ol.pagination__items > li")
-
+async def main():
     data = []
 
-    while True:
-        print("##########Scrapeando paginacion....##########")
-        data_pagination = extraer_info(productos, cant_productos)
-        data += data_pagination 
-        btn_next = page.locator("a.pagination__next")
-        
-        if btn_next.count() == 0:
-            print("Se acabo la paginacion")
-            break
-        
-        btn_next.click()
-        page.wait_for_selector(".srp-results", state="visible")
-    
-    crear_json(data, "ps4_2tb")   
-    input("Click para cerrar la pagina")
-    browser.close()
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+
+        await page.goto("https://www.ebay.com/")
+
+        buscador = page.get_by_placeholder("Buscar artículos")
+        await buscador.fill("ps4 2tb")
+        await buscador.press("Enter")
+
+        await page.wait_for_selector(".srp-results", state="visible")
+
+        while True:
+            print("########## Scrapeando paginación ##########")
+
+            productos = page.locator(".srp-results > li")
+            cant_productos = await productos.count()
+
+            data_pagination = await extraer_info(productos, cant_productos)
+            data += data_pagination
+
+            btn_next = page.locator("a.pagination__next")
+
+            if await btn_next.count() == 0:
+                print("Se acabó la paginación")
+                break
+
+            await btn_next.click()
+            await page.wait_for_selector(".srp-results", state="visible")
+
+        crear_json(data, "ps4_2tb")
+        await browser.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
