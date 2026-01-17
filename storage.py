f655d197f5bcbfd7ddc546c0797f83a2c5e469dd
Emmanuel Yapura <eyapura96@gmail.com>
2026-01-17 02:41:03 -0300

    agrega modelo de datos para archivo json

--- storage.py
+++ storage.py
@@ -1,5 +1,9 @@
 import json
+from typing import Iterable
+from models.product import Product
 
-def crear_json(datos: list[dict], filename: str) -> None:
+def crear_json(datos: Iterable[Product], filename: str) -> None:
+    data = [dato.model_dump() for dato in datos]
+
     with open(f"{filename}.json", "w", encoding="utf-8") as file :
-        json.dump(datos, file, indent=3, ensure_ascii=False)
+        json.dump(data, file, indent=3, ensure_ascii=False)
