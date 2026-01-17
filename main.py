8c17a3b4e998df369aa805e2d1311032e9b42769
Emmanuel Yapura <eyapura96@gmail.com>
2026-01-17 03:19:43 -0300

    agrega logger para json final y error en el scraper

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import asyncio
 from scraper import scrape_ebay, ScraperError
 from storage import crear_json
+from logger import setup_logger
+
+logger = setup_logger()
 
 async def main():
     try:
         products = await scrape_ebay("ps4 2tb")
         crear_json(products, "ps4_2tb")
+        logger.info(f"Archivo JSON generado correctamente")
 
     except ScraperError as e:
-        print(f"Error de scraping: {e}")
+        logger.error(f"Fallo el scraping: {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
