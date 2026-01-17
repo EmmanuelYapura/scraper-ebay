a9c08b714b6c13ac3faede0e1a87d6b90b62234f
Emmanuel Yapura <eyapura96@gmail.com>
2026-01-17 03:02:28 -0300

    controla errores con try except mediante la clase ScraperError

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import asyncio
-from scraper import scrape_ebay
+from scraper import scrape_ebay, ScraperError
 from storage import crear_json
 
 async def main():
-    products = await scrape_ebay("ps4 2tb")
-    crear_json(products, "ps4_2tb")
+    try:
+        products = await scrape_ebay("ps4 2tb")
+        crear_json(products, "ps4_2tb")
+
+    except ScraperError as e:
+        print(f"Error de scraping: {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
