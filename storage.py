f26837b45072b66e5b0e897a6d22a78d311a28a6
Emmanuel Yapura <eyapura96@gmail.com>
2026-01-17 02:25:03 -0300

    agrega annotations para la creacion del json

--- storage.py
+++ storage.py
@@ -1,5 +1,5 @@
 import json
 
-def crear_json(datos, filename):
+def crear_json(datos: list[dict], filename: str) -> None:
     with open(f"{filename}.json", "w", encoding="utf-8") as file :
         json.dump(datos, file, indent=3, ensure_ascii=False)
